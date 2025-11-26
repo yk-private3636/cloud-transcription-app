d7ef9e231fcbc4774ffd3e13d039b82133ca64f2
yk-private3636 <hsnysk318@gmail.com>
2025-11-26 15:46:08 +0900

    インフラストラクチャ構築(Bedrockバッチ推論ジョブ用 IAM作成)

---
+++ terraform/infra/envs/prod/bedrock_iam_policy.tf
@@ -0,0 +1,21 @@
+module "bedrock_iam_role_policy" {
+  source = "../../modules/iam_role_policy"
+
+  name    = local.bedrock_role_policy_name
+  role_id = module.bedrock_iam_role.id
+  policy_document = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = [
+          "${module.s3_bucket_bedrock_output.arn}/*",
+        ]
+      }
+    ]
+  })
+}
