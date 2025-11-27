e5b415b01a1ae1d15d72effe4aa64678c7638bf8
yk-private3636 <hsnysk318@gmail.com>
2025-11-27 18:48:57 +0900

    Transcribe出力結果取得 Lambda関数作成

---
+++ event/src/transcription_result_reader/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"transcription_result_reader/module"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+var (
+	reader module.Reader
+)
+
+type Event struct {
+	Bucket string `json:"bucket"`
+	Key    string `json:"key"`
+}
+
+type Output struct {
+	JobName    string `json:"jobName"`
+	AccountId  string `json:"accountId"`
+	Status     string `json:"status"`
+	Transcript string `json:"transcript"`
+}
+
+func init() {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	reader = module.NewReader(
+		s3.NewFromConfig(cfg),
+	)
+}
+
+func main() {
+	if strings.ToLower(os.Getenv("APP_ENV")) == "prod" {
+		lambda.Start(EventHandler)
+	} else {
+		event := module.FileContent("event.json")
+		fmt.Println(EventHandler(context.Background(), event))
+	}
+}
+
+func EventHandler(ctx context.Context, event json.RawMessage) (Output, error) {
+
+	var param Event
+	err := json.Unmarshal(event, &param)
+
+	if err != nil {
+		return Output{}, err
+	}
+
+	result, err := reader.TranscriptionResult(ctx, param.Bucket, param.Key)
+
+	if err != nil {
+		return Output{}, err
+	}
+
+	if len(result.Results.Transcripts) == 0 {
+		return Output{}, fmt.Errorf("transcript is empty")
+	}
+
+	return Output{
+		JobName:    result.JobName,
+		AccountId:  result.AccountId,
+		Status:     result.Status,
+		Transcript: result.Results.Transcripts[0].Transcript,
+	}, nil
+}
---
+++ event/src/transcription_result_reader/module/reader.go
@@ -0,0 +1,67 @@
+package module
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type reader struct {
+	client *s3.Client
+}
+
+type Reader interface {
+	TranscriptionResult(ctx context.Context, bucket, key string) (*ReaderResult, error)
+}
+
+type ReaderResult struct {
+	JobName   string `json:"jobName"`
+	AccountId string `json:"accountId"`
+	Status    string `json:"status"`
+	Results   struct {
+		Transcripts []struct {
+			Transcript string `json:"transcript"`
+		} `json:"transcripts"`
+	} `json:"results"`
+}
+
+func NewReader(client *s3.Client) Reader {
+	return &reader{
+		client: client,
+	}
+}
+
+func (r *reader) TranscriptionResult(ctx context.Context, bucket, key string) (*ReaderResult, error) {
+
+	obj, err := r.client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		obj.Body.Close()
+	}()
+
+	data, err := io.ReadAll(obj.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var result ReaderResult
+
+	err = json.Unmarshal(data, &result)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
